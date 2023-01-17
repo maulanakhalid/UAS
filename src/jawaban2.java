893047be5820cb38b34991dc68f7a0f61797671d
Maulana <Maulanakhalid27@gmaiil.com>
2023-01-17 12:35:04 +0700

    yes

---
+++ src/jawaban2.java
@@ -0,0 +1,18 @@
+public class jawaban2 {
+    public static void main(String[] args) {
+        System.out.println(ReverseString.Reverse("a b cd ef"));
+        System.out.println(ReverseString.Reverse("contoh kalimat"));
+    }
+    private static jawaban2 ReverseString;
+
+    public static String Reverse(String str) {
+        char ra[] = str.toCharArray();
+        String rev = "";
+        for (int i = ra.length-1;
+             i >= 0;
+             i--){
+            rev += ra[i];
+        }
+        return rev.replaceAll("(?)[aiueo]", "*");
+    }
+}
